6ba48f4cceb7b13098fc3637c5c224df521dcec7
yoshimura_jun <jun.yoshimura57@gmail.com>
2020-07-24 00:18:21 +0900

    #8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_11_025355) do
+ActiveRecord::Schema.define(version: 2020_07_23_134122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_07_11_025355) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_expences_on_user_id"
   end
 
@@ -31,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_07_11_025355) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false, null: false
     t.bigint "parent_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["parent_id"], name: "index_users_on_parent_id"
