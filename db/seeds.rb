adc358f681eb56b66acbb7e9d34333f51bf36abe
yoshimura_jun <jun.yoshimura57@gmail.com>
2020-07-26 20:46:53 +0900

    #9

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: '鈴木 親太郎',
+  email: 'admin@admin',
+  parent_id: '',
+  password: 'admin',
+  password_confirmation: 'admin'
+)
+
+User.create!(
+  name: '鈴木 子太郎',
+  email: 'kotaro@kotaro',
+  parent_id: '1',
+  password: 'kotaro',
+  password_confirmation: 'kotaro'
+)
+
+User.create!(
+  name: '鈴木 子二郎',
+  email: 'kojiro@kojiro',
+  parent_id: '1',
+  password: 'kojiro',
+  password_confirmation: 'kojiro'
+)
+
+User.create!(
+  name: '鈴木 子三郎',
+  email: 'kosaburo@kosaburo',
+  parent_id: '1',
+  password: 'kosaburo',
+  password_confirmation: 'kosaburo'
+)
