d30b0b1629f7e131db7f6fea4e4908ab8f688a70
junyoshimura57 <jun.yoshimura57@gmail.com>
2020-09-02 11:29:09 +0900

    #22

--- test/controllers/admin/children_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Admin::ChildrenControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get admin_children_new_url
-    assert_response :success
-  end
-
-end
